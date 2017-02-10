2d9aad7f91479c6c74787047f638fd80e4248acc
Nikolay Solodukhin <Nikolay.Solodukhin@gmail.com>
2017-02-10 23:45:12 +0300

    Переписал модули в IIFE

--- js/form.js
+++ js/form.js
@@ -1,29 +1,31 @@
 'use strict';
 
-var uploadForm = document.forms['upload-select-image'];
-var uploadFile = uploadForm['upload-file'];
+window.form = (function () {
+  var uploadForm = document.forms['upload-select-image'];
+  var uploadFile = uploadForm['upload-file'];
 
-var uploadOverlay = document.querySelector('.upload-overlay');
-var uploadFormCancel = uploadOverlay.querySelector('.upload-form-cancel');
+  var uploadOverlay = document.querySelector('.upload-overlay');
+  var uploadFormCancel = uploadOverlay.querySelector('.upload-form-cancel');
 
 
-var showUploadElement = function () {
-  uploadOverlay.classList.remove('invisible');
-  uploadForm.classList.add('invisible');
-};
+  var showUploadElement = function () {
+    uploadOverlay.classList.remove('invisible');
+    uploadForm.classList.add('invisible');
+  };
 
-var closeUploadElement = function () {
-  uploadOverlay.classList.add('invisible');
-  uploadForm.classList.remove('invisible');
-};
+  var closeUploadElement = function () {
+    uploadOverlay.classList.add('invisible');
+    uploadForm.classList.remove('invisible');
+  };
 
-uploadFile.addEventListener('change', function () {
-  showUploadElement();
-});
+  uploadFile.addEventListener('change', function () {
+    showUploadElement();
+  });
 
-uploadFormCancel.addEventListener('click', function () {
-  closeUploadElement();
-});
+  uploadFormCancel.addEventListener('click', function () {
+    closeUploadElement();
+  });
+})();
 
 window.initializeFilters();
 window.createScale(document.querySelector('.upload-resize-controls-value'), 25, 100);
--- js/initialize-filters.js
+++ js/initialize-filters.js
@@ -1,68 +1,69 @@
 'use strict';
 
-window.initializeFilters = function () {
 
-  var uploadFilterControls = document.querySelector('.upload-filter-controls');
-  var filterImage = document.querySelector('.filter-image-preview');
+window.initializeFilters = (function () {
+  return function () {
+    var uploadFilterControls = document.querySelector('.upload-filter-controls');
+    var filterImage = document.querySelector('.filter-image-preview');
 
-  var KEY_CODE = {
-    'ENTER': 13
-  };
+    var KEY_CODE = {
+      'ENTER': 13
+    };
 
-  var changeFilterValue = function () {
-    var filterMap;
-    if (!filterMap) {
+    var changeFilterValue = function () {
+      var filterMap;
+      if (!filterMap) {
 
-      filterMap = {
-        'none': 'filter-none',
-        'chrome': 'filter-chrome',
-        'sepia': 'filter-sepia',
-        'marvin': 'filter-marvin',
-        'phobos': 'filter-phobos',
-        'heat': 'filter-heat'
-      };
-    }
-    var selectedFilter = [].filter.call(uploadFilterControls.parentNode['upload-filter'], function (item) {
-      return item.checked;
-    })[0].value;
+        filterMap = {
+          'none': 'filter-none',
+          'chrome': 'filter-chrome',
+          'sepia': 'filter-sepia',
+          'marvin': 'filter-marvin',
+          'phobos': 'filter-phobos',
+          'heat': 'filter-heat'
+        };
+      }
+      var selectedFilter = [].filter.call(uploadFilterControls.parentNode['upload-filter'], function (item) {
+        return item.checked;
+      })[0].value;
 
-    filterImage.className = 'filter-image-preview ' + filterMap[selectedFilter];
-  };
+      filterImage.className = 'filter-image-preview ' + filterMap[selectedFilter];
+    };
 
-  uploadFilterControls.addEventListener('click', function (evt) {
-    filterChangerClick(evt);
-  });
+    uploadFilterControls.addEventListener('click', function (evt) {
+      filterChangerClick(evt);
+    });
 
-  uploadFilterControls.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === KEY_CODE.ENTER) {
-      filterChangerEnter(evt);
-    }
-  });
+    uploadFilterControls.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === KEY_CODE.ENTER) {
+        filterChangerEnter(evt);
+      }
+    });
 
-  var filterChangerClick = function (evt) {
-    if (evt.target.parentNode.classList[0] === 'upload-filter-label') {
-      evt.target.parentNode.control.checked = true;
+    var filterChangerClick = function (evt) {
+      if (evt.target.parentNode.classList[0] === 'upload-filter-label') {
+        evt.target.parentNode.control.checked = true;
+        changeFilterValue();
+        changeAriaChecked(evt);
+      }
+    };
+
+    var filterChangerEnter = function (evt) {
+      evt.target.control.checked = true;
       changeFilterValue();
       changeAriaChecked(evt);
-    }
-  };
+    };
 
-  var filterChangerEnter = function (evt) {
-    evt.target.control.checked = true;
-    changeFilterValue();
-    changeAriaChecked(evt);
+    var changeAriaChecked = function (evt) {
+      var labelElements = evt.currentTarget.querySelectorAll('.upload-filter-label');
+      for (var i = 0; i < labelElements.length; i++) {
+        labelElements[i].attributes['aria-checked'].nodeValue = false;
+      }
+      if (evt.type === 'click') {
+        evt.target.parentNode.attributes['aria-checked'].nodeValue = true;
+      } else if (evt.keyCode === KEY_CODE.ENTER) {
+        evt.target.attributes['aria-checked'].nodeValue = true;
+      }
+    };
   };
-
-  var changeAriaChecked = function (evt) {
-    var labelElements = evt.currentTarget.querySelectorAll('.upload-filter-label');
-    for (var i = 0; i < labelElements.length; i++) {
-      labelElements[i].attributes['aria-checked'].nodeValue = false;
-    }
-    if (evt.type === 'click') {
-      evt.target.parentNode.attributes['aria-checked'].nodeValue = true;
-    } else if (evt.keyCode === KEY_CODE.ENTER) {
-      evt.target.attributes['aria-checked'].nodeValue = true;
-    }
-  };
-
-};
+})();
